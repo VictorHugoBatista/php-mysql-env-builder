06fedb50b8c50c3b3147790b8b6a973fcba141f2
Victor Hugo <vct.bta@gmail.com>
2019-07-07 12:11:32 -0300

    Add the default data inputs do 'custom' command

--- src/commands/custom.ts
+++ src/commands/custom.ts
@@ -1,4 +1,5 @@
 import {Command} from '@oclif/command'
+const inquirer = require('inquirer')
 
 export default class Custom extends Command {
   static description = 'generate a custom php/mysql docker environment'
@@ -20,5 +21,39 @@ custom world from ./src/custom.ts!
   async run() {
     const {args} = this.parse(Custom)
     console.log(args.envname)
+    inquirer
+      .prompt([
+        {
+          type: 'name',
+          name: 'dbname',
+          message: 'Choose the database name',
+          default: args.envname,
+        },
+        {
+          type: 'name',
+          name: 'dbpass',
+          message: 'Choose the database root password',
+          default: 'root',
+        },
+        {
+          type: 'name',
+          name: 'vhost',
+          message: 'Choose the virtualhost',
+          default: `${args.envname}.local`,
+        },
+        {
+          type: 'name',
+          name: 'imageweb',
+          message: 'Choose the web server docker image',
+          default: 'lramos1994/docker-php-apache-base',
+        },
+        {
+          type: 'name',
+          name: 'imagedb',
+          message: 'Choose the database docker image',
+          default: 'mariadb',
+        },
+      ])
+      .then(answers => console.log(answers))
   }
 }
