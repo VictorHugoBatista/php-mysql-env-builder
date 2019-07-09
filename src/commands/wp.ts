5a05d18094f7f9cf4e3a78556f07df56842898ca
Victor Hugo <vct.bta@gmail.com>
2019-07-09 15:18:05 -0300

    Class Generator class on 'wp' command

--- src/commands/wp.ts
+++ src/commands/wp.ts
@@ -1,6 +1,8 @@
 import {Command} from '@oclif/command'
 const inquirer = require('inquirer')
 import {generateChoices} from '../choices/wp-command'
+import Generator from '../generator/generator'
+import {InputData} from '../interfaces/input-data'
 
 export default class Wp extends Command {
   static description = 'generate a php/mysql docker environment with an empty wordpress installation on the server root directory'
@@ -17,6 +19,8 @@ export default class Wp extends Command {
     const {args} = this.parse(Wp)
     inquirer
       .prompt(generateChoices(args.envname))
-      .then((answers: any) => console.log(answers))
+      .then((answers: InputData) => {
+        (new Generator(args.envname, answers)).generate()
+      })
   }
 }
