628a64a48b6ffa78a43807da1e1884715cf9169b
Victor Hugo <vct.bta@gmail.com>
2019-07-14 17:32:48 -0300

    Read docker-composer sample file content

--- src/sample-manager/sample-manager.ts
+++ src/sample-manager/sample-manager.ts
@@ -1,11 +1,14 @@
+const fs = require('fs')
 const path = require('path')
 
 export default class SampleManager {
-  private readonly _sampleFilesPath = path.join(__dirname, '..', '..', 'samples')
+  private readonly _sampleFilesPath: string = path.join(__dirname, '..', '..', 'samples')
   private readonly _sampleFilePath: string
+  private readonly _sampleFileContent: string
 
   public constructor(sampleFileName: string) {
     this._sampleFilePath = path.join(this._sampleFilesPath, sampleFileName)
-    console.log(this._sampleFilePath)
+    this._sampleFileContent = fs.readFileSync(this._sampleFilePath, 'utf8')
+    console.log(this._sampleFileContent)
   }
 }
