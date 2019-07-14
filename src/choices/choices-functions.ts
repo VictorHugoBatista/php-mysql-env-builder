605f7d3e90d0151731d1505500e7805c575c30d0
Victor Hugo <vct.bta@gmail.com>
2019-07-14 16:44:04 -0300

    Add makeFIeldUrl function to validate the repository inquirer questions

--- src/choices/choices-functions.ts
+++ src/choices/choices-functions.ts
@@ -1,1 +1,6 @@
+const isUrl = require('is-url')
+
 export const makeFieldRequired = (input: string): boolean => input !== ''
+
+export const makeFieldUrl = (input: string): boolean =>
+  input === '' || isUrl(input)
